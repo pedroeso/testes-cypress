7883fd8839c335c151ce5069982070ab48cbe5a9
Pedro Ribeiro <pedrorocha@gmail.com>
2025-03-10 16:46:41 -0300

    Primeiro Commit, adicionando os testes na inclusão!

---
+++ cypress/e2e/testes-agenda/adicionar.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Testes para a página de candidatura', () => {
+    beforeEach(() => {
+        cy.visit('https://agenda-contatos-react.vercel.app/')
+    })
+
+    it('Deve preencher os campos de inclusão', () => {
+        cy.get('input[type="text"]').type('Pedro Ribeiro')
+        cy.get('input[type="email"]').type('pedroesorocha@teste.com')
+        cy.get('input[type="tel"]').type('21 12345678')
+        cy.get('.adicionar').click()
+
+        cy.on('window:alert', (conteudo) => {
+            expect(conteudo).contain('Obrigado pelo novo contato!')
+        })
+
+        cy.screenshot('tela-inscricao-preenchido')
+    })
+})
